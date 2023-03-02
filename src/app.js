b25ea3c7ed14510add6069cf00652696552af83a
Sebastian Orozco <seba_orozco@hotmail.com>
2023-03-02 19:19:58 -0300

    Desafio Websockets + Handlebars

--- src/app.js
+++ src/app.js
@@ -1,13 +1,39 @@
 // Importar nuestras dependencias
 import express from 'express';
-import productsRouter from './router/products.routes.js';
-import cartsRouter from './router/carts.routes.js';
+import productsRouter from './routes/products.routes.js';
+import cartsRouter from './routes/carts.routes.js';
+import { engine } from 'express-handlebars';
+import __dirname from './utils.js';
+import ProductManager from './components/ProductManager.js';
 
 // Instanciar constantes
 const app = express();
+const product = new ProductManager;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+// ***** Configuración de Handlebars ********** //
+// Inicializamos el motor
+app.engine('handlebars', engine())
+
+// Luego indicamos que el motor ya inicializado arriba es el que queremos utilizar 
+app.set('view engine', 'handlebars');
+
+// Indicamos en qué parte del proyecto estarán las vistas
+app.set('views', __dirname + '/views');
+
+// Tenemos que definir las rutas estáticas de nuestras vistas
+app.use("/", express.static(__dirname + '/public'));
+
+// Enviamos la lista de todos los productos usando Handlebars
+app.get("/", async (req, res) => {
+    let allProducts = await product.getProducts();
+    res.render("home", {
+        title: "Cayena - Almacén Orgánico y Natural",
+        products: allProducts
+    })
+})
 
 // Vamos a crear las rutas de nuestros endpoints
 app.use('/api/products', productsRouter);
