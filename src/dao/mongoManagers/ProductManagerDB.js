23ab3ec8054fd21e05fd6e32c7f7b791519d0585
Sebastian Orozco <seba_orozco@hotmail.com>
2023-03-27 02:49:08 -0300

    Práctica Integradora

--- src/dao/mongoManagers/ProductManagerDB.js
+++ src/dao/mongoManagers/ProductManagerDB.js
@@ -1,9 +1,9 @@
-import { prodModel } from "../models/product.model";
+import { prodModel } from "../models/product.model.js";
 
 class ProductManagerDB {
 
     //CREO UN PRODUCTO 
-      static async create(req, res) {
+    static async create(req, res) {
        const { body, /*file*/ } = req
        const imagenProducto = {
           ...body,
@@ -11,36 +11,41 @@ class ProductManagerDB {
         }
         const newProduct = await prodModel.create(imagenProducto)
         res.status(201).json(newProduct)
-      }
+    }
 
     //OBTENGO LOS PRODUCTOS
-      static async get(req, res) {
-        const result = await prodModel.find()
-        res.status(200).json(result)
-      }
+    static async get(req, res) {
+        try {
+            let products = await prodModel.find();
+            res.status(200).json(products);
+        } catch (error) {
+            console.log("Cannot get products with mongoose: "+ error);
+        }
+    }
 
     //OBTENGO UN PRODUCTO POR ID
-      static async getById(req, res) {
-        const { params: { id } } = req
-        const result = await prodModel.findById(id)
+    static async getById(req, res) {
+        const { params: { pid } } = req
+        const result = await prodModel.findById(pid)
         if (!result) {
           return res.status(404).end()
         }
         res.status(200).json(result)
-      }
+    }
 
     //MODIFICO UN PRODUCTO POR ID
-      static async updateById(req, res) {
-        const { params: { id }, body } = req
-        await prodModel.updateOne({ _id: id }, { $set: body })
-        res.status(204).end()
-      }
+    static async updateById(req, res) {
+        const { params: { pid }, body } = req;
+        const updateProduct = await prodModel.updateOne({ _id: pid }, { $set: body });
+        res.status(204).json(updateProduct);
+    }
+
     //ELIMINO UN PRODUCTO POR ID
-      static async deleteById(req, res) {
-        const { params: { id } } = req
-        await prodModel.deleteOne({ _id: id })
-        res.status(204).end()
-      }
+    static async deleteById(req, res) {
+        const { params: { id } } = req;
+        const deleteProduct = await prodModel.deleteOne({ _id: pid });
+        res.status(204).json(deleteProduct);
+    }
     
 }
     
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,37 +1,20 @@
 import { Router } from "express";
 import { prodModel } from "../dao/models/product.model.js";
+import ProductManagerDB from "../dao/mongoManagers/productManagerDB.js";
 
 const router = Router();
 
 //CREATE
-router.post('/products', async (req, res) => {
-    const { body } = req;
-    const newProduct = await prodModel.create(body);
-    res.status(201).json(newProduct);
-})
+router.post('/products', ProductManagerDB.create);
 
 // READ
-router.get('/products', async (req, res) => {
-    try {
-        let products = await prodModel.find();
-        res.status(200).json(products);
-    } catch (error) {
-        console.log("Cannot get products with mongoose: "+ error);
-    }
-})
+router.get('/products', ProductManagerDB.get);
+router.get('/products/:pid', ProductManagerDB.getById);
 
 // UPDATE
-router.put('/products/:pid', async (req, res) => {
-    const { params: { pid }, body } = req;
-    const updateProduct = await prodModel.updateOne({ _id: pid }, { $set: body });
-    res.status(204).json(updateProduct);
-})
+router.put('/products/:pid', ProductManagerDB.updateById);
 
 // DELETE
-router.delete('/products/:pid', async (req, res) => {
-    const { params: { pid } } = req;
-    const deleteProduct = await prodModel.deleteOne({ _id: pid });
-    res.status(204).json(deleteProduct);
-})
+router.delete('/products/:pid', ProductManagerDB.deleteById)
 
 export default router;
